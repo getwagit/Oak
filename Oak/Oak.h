5a5842d7361c685a0926d174a07cf98a645f3e28
Markus Riegel <riegel.markus@googlemail.com>
2016-08-22 17:25:17 +0200

    (add) initial code

---
+++ Oak/Oak.h
@@ -0,0 +1,15 @@
+//
+//  Created by Markus Riegel on 22.08.16.
+//  Copyright © 2016 wag it GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Oak.
+FOUNDATION_EXPORT double OakVersionNumber;
+
+//! Project version string for Oak.
+FOUNDATION_EXPORT const unsigned char OakVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Oak/PublicHeader.h>
+
